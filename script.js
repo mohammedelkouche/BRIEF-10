d53b2eca94c5013f8f629eb1c2540c52b60cabd2
mohammedelkouche <mohammed0elkouche@gmail.com>
2022-01-19 22:24:12 +0100

    methode 2 pushdata

--- script.js
+++ script.js
@@ -7,22 +7,61 @@ $(document).ready(function(){
     // jQuery methods go here...
     // jQuery AJAX Methods
 
+    // $.getJSON("phones.json",function(data){
+    //     var info_data = data.phones ;
+    //     console.log(info_data)
+    //     var pushdata = "" ;
+    //     $.each(info_data,function(key,value){
+    //         console.log(key)
+    //         pushdata += '<tr>';
+    //         pushdata += '<td>'+value.ID+'</td>';
+    //         pushdata += '<td>'+value.désignation+'</td>';
+    //         pushdata += '<td>'+value.prix+'</td>';
+    //         pushdata += '<td>'+value.catégorie+'</td>';
+    //         pushdata += '<td>'+value.disponibilité+'</td>';
+    //         pushdata += '<td><li>'+value.fournisseur.Raison+'</li>'+'<li>'+value.fournisseur.Adresse+'</li><td>';
+
+    //         pushdata += '</tr>';
+    //     });
+    //     $('#table_body').append(pushdata);
+    // });
+
+
+    
     $.getJSON("phones.json",function(data){
         var info_data = data.phones ;
         console.log(info_data)
         var pushdata = "" ;
         $.each(info_data,function(key,value){
-            console.log(value)
-            pushdata += '<tr>';
-            pushdata += '<td>'+value.ID+'</td>';
-            pushdata += '<td>'+value.désignation+'</td>';
-            pushdata += '<td>'+value.prix+'</td>';
-            pushdata += '<td>'+value.catégorie+'</td>';
-            pushdata += '<td>'+value.disponibilité+'</td>';
-            pushdata += `<td><li>${value.fournisseur.Raison}</li><li>${value.Adresse}</li><td>`;
+                    pushdata += `<tr>
+                    <td>${value.ID}</td>
+                    <td>${value.désignation}</td>
+                    <td>${value.prix}</td>
+                    <td>${value.catégorie}</td>
+                    <td>${value.disponibilité}</td>
+                    <td><li>${value.fournisseur.Raison}</li><li>${value.fournisseur.Adresse}</li><td>
+                    </tr>`
 
-            pushdata += '</tr>';
+                    /*
+                    if we use append pushdata in table_body old and new lobjet
+
+                                        $('#table_body').append(pushdata);
+
+                    witsh we stock in pushdata will be added to the table_body 
+                    so we should place outside "each" or we use innerHTML  inside each 
+                    */
+
+
+                    // table_body.innerHTML = pushdata ;
+
+
         });
-        $('#table').append(pushdata);
+        // $('#table_body').append(pushdata);
+        table_body.innerHTML = pushdata ;
+
+
     });
+
+
+         
 })
